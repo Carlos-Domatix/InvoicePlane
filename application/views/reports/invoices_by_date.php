a0614c57fa9ad0efb332ae8d2020af48d087aeaf
Carlos Alba <carlos.alba@domatix.com>
2016-01-08 13:38:38 +0100

    WIP: Invoices by date

---
+++ application/views/reports/invoices_by_date.php
@@ -0,0 +1,10 @@
+<html>
+	<head>
+		<title><?php echo lang('invoices_by_date'); ?></title>
+		<link rel="stylesheet" href="<?php echo base_url(); ?>assets/default/css/reports.css" type="text/css">
+	</head>
+
+	<body>
+		
+	</body>
+</html>
